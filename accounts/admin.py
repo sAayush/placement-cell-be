c9c269bb43a3795cc50c8f60bd7b0ddf6f76568a
paras-2407 <parasgoyal@jklu.edu.in>
2024-06-05 18:27:11 +0530

    added files

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+
+    model = CustomUser
+    list_display = ["phone_number","email", "username","name"]
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED,HTTP_200_OK
+from .serializers import UserSignUpSerializer, UserSignInSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from django.urls import reverse_lazy
+# from django.views.generic.edit import CreateView
+    
+
+
+User = get_user_model()
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = UserSignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class SignInView(APIView):
+    def post(self, request):
+        serializer = UserSignInSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_401_UNAUTHORIZED)
+    
